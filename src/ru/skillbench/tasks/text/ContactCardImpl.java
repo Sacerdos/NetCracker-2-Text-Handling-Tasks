2e39a63376b011d2272cf84a69f80e7e7b689657
Ilya Dychkov <ilya.dychkov@gmail.com>
2019-03-24 02:18:34 +0300

    Update to ContactCardImpl

--- src/ru/skillbench/tasks/text/ContactCardImpl.java
+++ src/ru/skillbench/tasks/text/ContactCardImpl.java
@@ -1,4 +1,54 @@
 package ru.skillbench.tasks.text;
 
-public class ContactCardImpl {
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ContactCardImpl implements ContactCard{
+    @Override
+    public ContactCard getInstance(Scanner scanner){
+
+    }
+    @Override
+    public ContactCard getInstance(String data){
+
+    }
+
+    @Override
+    public String getFullName(){
+
+    }
+
+    @Override
+    public String getOrganization(){
+
+    }
+
+    @Override
+    public boolean isWoman(){
+
+    }
+
+    @Override
+    public Calendar getBirthday(){
+
+    }
+
+    @Override
+    public Period getAge(){
+
+    }
+
+    @Override
+    public int getAgeYears(){
+
+    }
+
+    @Override
+    public String getPhone(String type){
+
+    }
 }
