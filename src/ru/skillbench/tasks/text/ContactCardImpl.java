9c2b3b2dcb0a69f80120bc0749d512e62db3ea5f
Ilya Dychkov <ilya.dychkov@gmail.com>
2019-03-28 01:48:26 +0300

    Update to ContactCardImpl

--- src/ru/skillbench/tasks/text/ContactCardImpl.java
+++ src/ru/skillbench/tasks/text/ContactCardImpl.java
@@ -1,120 +1,162 @@
 package ru.skillbench.tasks.text;
 
-import java.util.regex.*;
-import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Period;
-import java.util.Calendar;
-import java.util.InputMismatchException;
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
 
-public class ContactCardImpl implements ContactCard {
+public class ContactCardImpl implements ContactCard{
     private String fullName;
     private String organization;
     private String gender;
-    public Calendar birthday;
-    private String[][] phoneNumber = new String[5][2];
+    private Calendar birthday;
+    private ArrayList<String> phoneNumber = new ArrayList<>(0);
+    private ArrayList<String> typeNumber = new ArrayList<>(0);;
 
-    public ContactCard getInstance(Scanner scanner) {
+
+    @Override
+    public ContactCard getInstance(Scanner scanner){
         LinkedList<String> values = new LinkedList<String>();
         while (scanner.hasNextLine()) {
             values.add(scanner.nextLine());
-
         }
-        if (values.size() < 2 || values.get(0).compareTo("BEGIN:VCARD")!=0) {
-
+        if (values.size() < 4 || values.get(0).compareTo("BEGIN:VCARD")!=0 || values.get(1).length()<4 || values.get(2).length()<5 || values.get(values.size()-1).compareTo("END:VCARD")!=0) {
             throw new NoSuchElementException();
         }
-        System.out.println(scanner.toString(	));
-        System.out.println(values);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-        try {
-            Date date = sdf.parse(values.get(4).split(":")[1]);
-            Calendar cal = sdf.getCalendar();
-            birthday = cal;
-            try{
-                if (!values.get(3).split(":")[1].equals("F") && !values.get(3).split(":")[1].equals("M")) {
-                    throw new InputMismatchException();
-                }
-                gender = values.get(3).split(":")[1];
-            } catch (ArrayIndexOutOfBoundsException ex){
-                System.out.println(values.size() + " " + values.get(0));
-            }
-
-        } catch (ParseException e) {
+        if (values.get(1).charAt(2)!=':' || values.get(2).charAt(3)!=':') {
             throw new InputMismatchException();
         }
-        fullName = values.get(1).split(":")[1];
-        organization = values.get(2).split(":")[1];
-        gender = "N";
-        int i = 5;
-        while (this.searchPhone(i, values)) {
-            i++;
+        String[] fnString = values.get(1).split(":");
+        String[] orgString = values.get(2).split(":");
+        if(fnString[0].compareTo("FN")!=0 || orgString[0].compareTo("ORG")!=0){
+            throw new InputMismatchException();
         }
+        fullName=fnString[1];
+        organization=orgString[1];
+        /*for(int i=0;i<values.size();i++){
+            System.out.println(values.get(i));
+        }*/
+        int itNum=0;
+        if(values.size()>4){
+            for(int j=3;j<values.size()-1;j++){
+                String[] temp = values.get(j).split(":");
+                if(temp[0].compareTo("GENDER")==0 && values.get(j).charAt(6)==':'){
+                    if(temp[1].compareTo("M")==0){
+                        gender="M";
+                    } else if (temp[1].compareTo("F")==0){
+                        gender="F";
+                    } else if (temp.length==1){
+                        gender="";
+                    } else {
+                        throw new InputMismatchException();
+                    }
+                }
+                if(temp[0].compareTo("BDAY")==0 && values.get(j).charAt(4)==':'){
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                    String[] tempDate = temp[1].split("-");
+
+                    try {
+                        if(tempDate.length!=3 || Integer.parseInt(tempDate[0])>31 || (Integer.parseInt(tempDate[0])<1) || (Integer.parseInt(tempDate[1])>12) || (Integer.parseInt(tempDate[1])<1) || (Integer.parseInt(tempDate[2])>2018)){
+                            throw new InputMismatchException();
+                        }
+                        Date date = sdf.parse(temp[1]);
+                        Calendar calendar = sdf.getCalendar();
+                        birthday = calendar;
+                    } catch (ParseException e) {
+                        throw new InputMismatchException();
+                    }
+                    //System.out.println(birthday.get(5) + "-" + (birthday.get(2)+1) + "-" + birthday.get(1));
+                }
+                if(temp[0].split("=").length>1){
+                    String[] tempNum = temp[0].split("=");
+                    if(tempNum[0].compareTo("TEL;TYPE")==0){
+                        String reg = "^[a-zA-Z0-9]*$";
+                        //Pattern pg = Pattern.compile(reg, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+                        //System.out.println(typeNumber);
+                        if(temp[1].length()!=10 || !temp[1].matches(reg)){
+                            throw new InputMismatchException();
+                        }
+                        //Matcher match = pg.matcher(temp[1]);
+                        //System.out.println(match);
+                        String[] tempType = tempNum[1].split(",");
+                        boolean check = false;
+                        for(int i=0; i<typeNumber.size();i++){
+                            if(typeNumber.get(i).compareTo(tempType[0])==0){
+                                check = true;
+                            }
+                        }
+                        if (!check){
+                            typeNumber.add(tempType[0]);
+                            String num = temp[1];
+                            phoneNumber.add("("+num.charAt(0)+num.charAt(1)+num.charAt(2)+") " +num.charAt(3)+num.charAt(4)+num.charAt(5)+"-"+num.charAt(6)+num.charAt(7)+num.charAt(8)+num.charAt(9) );
+                        }
+
+
+                    } else {
+                        throw new InputMismatchException();
+                    }
+                }
+
+
+            }
+        }
+        //System.out.println(typeNumber);
         return this;
     }
-    public ContactCard getInstance(String data) {
-        Scanner sc = new Scanner(data);
-        return this.getInstance(sc);
+    @Override
+    public ContactCard getInstance(String data){
+        return this.getInstance(new Scanner(data));
     }
-
-    public String getFullName() {
+    public String getFullName(){
         return fullName;
     }
-
-    public String getOrganization() {
+    public String getOrganization(){
         return organization;
     }
-
-    public boolean isWoman() {
-        if (gender == "F") {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean isWoman(){
+        return gender=="F";
     }
-
-    public Calendar getBirthday() {
+    public Calendar getBirthday(){
+        if(birthday==null){
+            throw new NoSuchElementException();
+        }
         return birthday;
     }
 
-    public Period getAge() {
+
+    public Period getAge(){
+        if(birthday==null){
+            throw new NoSuchElementException();
+        }
         int year = Calendar.getInstance().get(Calendar.YEAR);
         int byear = birthday.get(Calendar.YEAR);
+        //System.out.println(year + " "+ byear);
         Period age = Period.ofYears(year - byear);
         return age;
     }
 
-    public int getAgeYears() {
+    public int getAgeYears(){
+        if(birthday==null){
+            throw new NoSuchElementException();
+        }
         int year = Calendar.getInstance().get(Calendar.YEAR);
         int byear = birthday.get(Calendar.YEAR);
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
+        int bday = birthday.get(Calendar.DAY_OF_YEAR);
+        if(day<bday){
+            return year - byear-1;
+        }
         return year - byear;
     }
 
-    public String getPhone(String type) {
-        String res = new String();
-        for (int i = 0; i < 5; i++) {
-            if (phoneNumber[i][0].equals(type)) {
-                return "(" + phoneNumber[i][1].substring(0, 3) + ") " + phoneNumber[i][1].substring(3, 6) + "-" + phoneNumber[i][1].substring(6, 10);
-            }
-        }
-        return res;
-    }
+   public String getPhone(String type){
+       for(int i=0;i<typeNumber.size();i++){
+           if(typeNumber.get(i).compareTo(type)==0){
+               return phoneNumber.get(i);
+           }
+       }
+       throw new NoSuchElementException();
+   }
 
-    public boolean searchPhone(int numOfIter, LinkedList<String> values) {
-        String reg = ".*?(?:[a-z][a-z]+).*?(?:[a-z][a-z]+).*?((?:[a-z][a-z]+)).*?(\\d+)";
-        Pattern pg = Pattern.compile(reg, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-        Matcher mg1 = pg.matcher(values.get(numOfIter));
-        if (mg1.find()) {
-            String wordg1 = mg1.group(1);
-            String int1 = mg1.group(2);
-            phoneNumber[numOfIter - 5][0] = wordg1;
-            phoneNumber[numOfIter - 5][1] = String.valueOf(int1);
-        }
-        Matcher mg2 = pg.matcher(values.get(numOfIter + 1));
-        return mg2.find();
-    }
+    //}
 }
--- src/ru/skillbench/tasks/text/Main.java
+++ src/ru/skillbench/tasks/text/Main.java
@@ -2,17 +2,15 @@ package ru.skillbench.tasks.text;
 public class Main {
 	public static void main(String[] args) {
 		ContactCard cc = new ContactCardImpl();
-		cc.getInstance("BEGIN:VCARD\r\nFN:Forrest Gump\r\nORG:Bubba Gump Shrimp Co.\r\nGENDER:\r\nBDAY:06-06-1944\r\nTEL;TYPE=WORK,VOICE:4951234567\r\nTEL;TYPE=CELL,VOICE:9150123456\r\nEND:VCARD");
-		//System.out.println(cc.getFullName());
-		//System.out.println(cc.getOrganization());
+		cc.getInstance("BEGIN:VCARD\r\nFN:Forrest Gump\r\nORG:Bubba Gump Shrimp Co.\r\nGENDER:M\r\nBDAY:24-11-1991\r\nTEL;TYPE=HOME:1234567890\r\nEND:VCARD");
+		//cc.getInstance("BEGIN:VCARD\r\nFN:Forest\r\nORG:Bubba Gump Shrimp Co.\r\nGENDER:M\r\nBDAY:32-58-1944\r\nEND:VCARD");
+		System.out.println(cc.getFullName());
+		System.out.println(cc.getOrganization());
 		System.out.println(cc.isWoman());
-		//System.out.println(cc.getBirthday());
-		//System.out.println(cc.getAgeYears());
-		//System.out.println(((ContactCardImpl) cc).birthday.get(Calendar.YEAR));
-		//System.out.println(((ContactCardImpl) cc).birthday.get(Calendar.MONTH));
-		//System.out.println(((ContactCardImpl) cc).birthday.get(Calendar.DAY_OF_MONTH));
-		//System.out.println(cc.getAge().getYears());
-		System.out.println(cc.getPhone("CELL"));
+		System.out.println(cc.getBirthday());
+		System.out.println(cc.getAgeYears());
+		System.out.println(cc.getAge());
+		System.out.println(cc.getPhone("WORK"));
 
 
 	}
